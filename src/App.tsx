41e660ac4987c6fa367448e1ab3e493ad1577adf
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2022-06-23 21:13:29 -0300

    Aula 3. Carregando dados do graphcms

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { gql, useQuery } from "@apollo/client"
-import { Event } from "./pages/Event";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }`
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
 
   interface Lesson {
     id: string;
     title: string;
   }
 
+//react-router-dom também tem seu próprio provider que é o BrowserRouter
+
 function App() {
   
   return (
-   <Event/>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client' //react-dom pois é react para web
 import App from './App'
-import { client } from './lib/apollo'
 import './styles/global.css'
 
 //createRoot método inicial chamado uma única vez na aplicação
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
